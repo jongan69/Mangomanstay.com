c312d38a708d1ab694dc1b470810a97f48b9d807
jongan69 <jonathang132298@gmail.com>
2018-02-03 16:06:46 -0500

    auth

--- Mangomanstay React/Database.js
+++ Mangomanstay React/Database.js
@@ -12,6 +12,7 @@ Alcohol drinking behavior[3]= ["Things to-do","https://visitsedona.com/blog/wini
 Alcohol drinking behavior[4]= ["Food&Beverage","https://www.cruisecritic.com/articles.cfm?ID=1470","http://cdn.newsapi.com.au/image/v1/4c5991420d00b52425d252b0407f6f11"];
 	
 
+content["anger"] = [];
 //Anger
 Anger[0] = ["Things to-do","https://disneyworld.disney.go.com/planning-guides/in-depth-advice/first-trip/","https://s-media-cache-ak0.pinimg.com/originals/a0/b4/1a/a0b41ac2524b2ab9247232a6740f4a07.jpg"];
 Anger[1] = ["Things to-do","https://www.infinityfitness.com/news-and-store/blog/cruise-stress-relief-options/","http://www.trbimg.com/img-54c132de/turbine/os-frozen-disney-cruise-line-20150122"];
@@ -20,84 +21,114 @@ Anger[3] = [Travel Tip,"https://www.huffingtonpost.com/entry/how-to-have-a-stres
 Anger[4] = ["Things to-do","https://blog.freepeople.com/2013/09/blogger-diary-nature-walk/","http://www.connerprairie.org/Places-To-Explore/Images-/Nature-Walk/DSC_1602.jpg"];
 
 
+content["Caffeine-consumption"] = [];
 //Caffeine Consumption
-Caffeine Consumption[0,Food&Beverage,"https://www.cntraveler.com/stories/2015-01-03/americas-10-best-juice-bars-for-your-new-years-cleanse","https://i1.wp.com/www.homeandheavens.com/wp-content/uploads/2014/04/Island-juice-bar-designs_2.jpg?w=600"];
-Caffeine Consumption[1,Food&Beverage,"https://www.huffingtonpost.com/food-wine/best-soda-fountains-in-th_b_5890634.html","http://dallas.culturemap.com/thumbnail/600x600/photos/2013/04/12/Soda-Bar-at-NYLO-South-Side_170955.jpg"];
-Caffeine Consumption[2,Products,"https://www.consumerreports.org/cro/coffee-makers.htm","http://s3.amazonaws.com/digitaltrends-uploads-prod/2016/11/best-coffee-makers.jpg"];
-Caffeine Consumption[3,Food&Beverage,"http://www.travelchannel.com/interests/food-and-drink/photos/worlds-best-cafes-for-coffee-lovers","http://41.media.tumblr.com/5df0747728a28202a62845ef96ead780/tumblr_nikzlsQA9p1tu9sqto1_r1_500.png"];
-Caffeine Consumption[4,,"",""];
+Caffeine Consumption[0] = [Food&Beverage,"https://www.cntraveler.com/stories/2015-01-03/americas-10-best-juice-bars-for-your-new-years-cleanse","https://i1.wp.com/www.homeandheavens.com/wp-content/uploads/2014/04/Island-juice-bar-designs_2.jpg?w=600"];
+Caffeine Consumption[1] = [Food&Beverage,"https://www.huffingtonpost.com/food-wine/best-soda-fountains-in-th_b_5890634.html","http://dallas.culturemap.com/thumbnail/600x600/photos/2013/04/12/Soda-Bar-at-NYLO-South-Side_170955.jpg"];
+Caffeine Consumption[2] = [Products,"https://www.consumerreports.org/cro/coffee-makers.htm","http://s3.amazonaws.com/digitaltrends-uploads-prod/2016/11/best-coffee-makers.jpg"];
+Caffeine Consumption[3] = [Food&Beverage,"http://www.travelchannel.com/interests/food-and-drink/photos/worlds-best-cafes-for-coffee-lovers","http://41.media.tumblr.com/5df0747728a28202a62845ef96ead780/tumblr_nikzlsQA9p1tu9sqto1_r1_500.png"];
+Caffeine Consumption[4] = [,"",""];
 
 
+content["Depression"] = [];
 //Depression
-Depression[0,Things to-do,"https://www.volunteerforever.com/article_post/2017-best-volunteer-abroad-programs-organizations-projects","https://www.professionalgrantwriter.org/wp-content/uploads/2011/07/volunteer-hours-budget-nonprofit.jpg"];
-Depression[1,Food&Beverage,"https://makeyourbodywork.com/best-healthy-food-blogs/","https://www.naturalproductsinsider.com/~/media/8bfde384f9ea49dda5dd8a5205d583f4.ashx"];
-Depression[2,Things to-do,"https://www.prevention.com/fitness/14-walking-workouts-to-burn-fat-and-boost-energy","https://img.aws.livestrongcdn.com/ls-article-image-673/ds-photo/getty/article/81/16/78616903.jpg"];
-Depression[3,Things to-do,"blog.anytimefitness.com/","http://www.tyrolcleaning.com/wp-content/uploads/2012/10/pittsburgh-gym-fitness-cleaning.jpg"];
-Depression[4,Things to-do,"http://trupanion.com/blog/2017/11/7-innovative-therapy-dog-programs/","https://www.gannett-cdn.com/-mm-/83ade179910f010dab8e5f2674eb2a62ea7f7a81/c=169-1205-1446-1927&r=x803&c=1600x800/local/-/media/2015/12/07/Hattiesburg/Hattiesburg/635851049992401523-Puppy-Therapy-7.jpg"];
+Depression[0] = [Things to-do,"https://www.volunteerforever.com/article_post/2017-best-volunteer-abroad-programs-organizations-projects","https://www.professionalgrantwriter.org/wp-content/uploads/2011/07/volunteer-hours-budget-nonprofit.jpg"];
+Depression[1] = [Food&Beverage,"https://makeyourbodywork.com/best-healthy-food-blogs/","https://www.naturalproductsinsider.com/~/media/8bfde384f9ea49dda5dd8a5205d583f4.ashx"];
+Depression[2] = [Things to-do,"https://www.prevention.com/fitness/14-walking-workouts-to-burn-fat-and-boost-energy","https://img.aws.livestrongcdn.com/ls-article-image-673/ds-photo/getty/article/81/16/78616903.jpg"];
+Depression[3] = [Things to-do,"blog.anytimefitness.com/","http://www.tyrolcleaning.com/wp-content/uploads/2012/10/pittsburgh-gym-fitness-cleaning.jpg"];
+Depression[4] = [Things to-do,"http://trupanion.com/blog/2017/11/7-innovative-therapy-dog-programs/","https://www.gannett-cdn.com/-mm-/83ade179910f010dab8e5f2674eb2a62ea7f7a81/c=169-1205-1446-1927&r=x803&c=1600x800/local/-/media/2015/12/07/Hattiesburg/Hattiesburg/635851049992401523-Puppy-Therapy-7.jpg"];
 		
+
+content["Excessive-daytime-sleepiness"] = [];
 //Excessive Daytime Sleepiness
-Excessive Daytime Sleepiness[0,Travel Tip,"https://www.webmd.com/sleep-disorders/features/natural-tips-sleepiness#1","https://33q47o1cmnk34cvwth15pbvt120l-wpengine.netdna-ssl.com/wp-content/uploads/insomnia.jpg"];
-Excessive Daytime Sleepiness[1,Things to-do,"https://greatist.com/fitness/take-it-easy-21-unexpected-low-impact-workouts","http://www.qtfitness.me/wp-content/uploads/2014/10/workout-park-friends-1.jpg"];
-Excessive Daytime Sleepiness[2,Food&Beverage,"https://www.livescience.com/36579-eat-meals.html","https://static.independent.co.uk/s3fs-public/styles/article_small/public/thumbnails/image/2017/11/15/15/eating-quickly.jpg"];
-Excessive Daytime Sleepiness[3,];
-Excessive Daytime Sleepiness[4,];
+Excessive Daytime Sleepiness[0] = [Travel Tip,"https://www.webmd.com/sleep-disorders/features/natural-tips-sleepiness#1","https://33q47o1cmnk34cvwth15pbvt120l-wpengine.netdna-ssl.com/wp-content/uploads/insomnia.jpg"];
+Excessive Daytime Sleepiness[1] = [Things to-do,"https://greatist.com/fitness/take-it-easy-21-unexpected-low-impact-workouts","http://www.qtfitness.me/wp-content/uploads/2014/10/workout-park-friends-1.jpg"];
+Excessive Daytime Sleepiness[2] = [Food&Beverage,"https://www.livescience.com/36579-eat-meals.html","https://static.independent.co.uk/s3fs-public/styles/article_small/public/thumbnails/image/2017/11/15/15/eating-quickly.jpg"];
+Excessive Daytime Sleepiness[3];
+Excessive Daytime Sleepiness[4];
 
+
+content["Extraversion"] = [];
 //Extraversion
-Extraversion[0,Misc,"https://www.quietrev.com/these-travel-destinations-prove-that-quiet-doesnt-mean-boring/","https://33q47o1cmnk34cvwth15pbvt120l-wpengine.netdna-ssl.com/wp-content/uploads/insomnia.jpg"];
-Extraversion[1,Things to-do,"https://greatist.com/fitness/take-it-easy-21-unexpected-low-impact-workouts","http://www.qtfitness.me/wp-content/uploads/2014/10/workout-park-friends-1.jpg"];
-Extraversion[2,];
-Extraversion[3,];
-Extraversion[4,];
+Extraversion[0] = [Misc,"https://www.quietrev.com/these-travel-destinations-prove-that-quiet-doesnt-mean-boring/","https://33q47o1cmnk34cvwth15pbvt120l-wpengine.netdna-ssl.com/wp-content/uploads/insomnia.jpg"];
+Extraversion[1] = [Things to-do,"https://greatist.com/fitness/take-it-easy-21-unexpected-low-impact-workouts","http://www.qtfitness.me/wp-content/uploads/2014/10/workout-park-friends-1.jpg"];
+Extraversion[2];
+Extraversion[3];
+Extraversion[4];
 			 
+
+content["Gambling"] = [];
 //Gambling
-Gambling[0,];
-Gambling[1,];
-Gambling[2,];
-Gambling[3,];
-Gambling[4,];
+Gambling[0];
+Gambling[1];
+Gambling[2];
+Gambling[3];
+Gambling[4];
 		 
+
+content["Carbohydrate-Intake"] = [];
 //Carbohydrate Intake
-Carbohydrate Intake[0,];
-Carbohydrate Intake[1,];
-Carbohydrate Intake[2,];
-Carbohydrate Intake[3,];
-Carbohydrate Intake[4,];
+Carbohydrate Intake[0];
+Carbohydrate Intake[1];
+Carbohydrate Intake[2];
+Carbohydrate Intake[3];
+Carbohydrate Intake[4];
 
+
+content["Nausea"] = [];
 //Nausea
-Nausea[0,];
-Nausea[1,];
-Nausea[2,];
-Nausea[3,];
-Nausea[4,];
+Nausea[0];
+Nausea[1];
+Nausea[2];
+Nausea[3];
+Nausea[4];
 	   
+
+	   
+content["Skin-Pigmentation"] = [];	   
 //Skin Pigmentation
-Skin Pigmentation[0,Product,"http://amzn.to/2nFNFTl","https://images-na.ssl-images-amazon.com/images/I/518unQP%2Bo3L._SL160_.jpg"];
-Skin Pigmentation[1,Product,"http://amzn.to/2nFNFTl","https://images-na.ssl-images-amazon.com/images/I/518unQP%2Bo3L._SL160_.jpg"];
-Skin Pigmentation[2,Product,"http://amzn.to/2nFNFTl","https://images-na.ssl-images-amazon.com/images/I/518unQP%2Bo3L._SL160_.jpg"];
-Skin Pigmentation[3,Product,"http://amzn.to/2nFNFTl","https://images-na.ssl-images-amazon.com/images/I/518unQP%2Bo3L._SL160_.jpg"];
-Skin Pigmentation[4,Product,"http://amzn.to/2nFNFTl","https://images-na.ssl-images-amazon.com/images/I/518unQP%2Bo3L._SL160_.jpg"];
+Skin Pigmentation[0] = [Product,"http://amzn.to/2nFNFTl","https://images-na.ssl-images-amazon.com/images/I/518unQP%2Bo3L._SL160_.jpg"];
+Skin Pigmentation[1] = [Product,"http://amzn.to/2nFNFTl","https://images-na.ssl-images-amazon.com/images/I/518unQP%2Bo3L._SL160_.jpg"];
+Skin Pigmentation[2] = [Product,"http://amzn.to/2nFNFTl","https://images-na.ssl-images-amazon.com/images/I/518unQP%2Bo3L._SL160_.jpg"];
+Skin Pigmentation[3] = [Product,"http://amzn.to/2nFNFTl","https://images-na.ssl-images-amazon.com/images/I/518unQP%2Bo3L._SL160_.jpg"];
+Skin Pigmentation[4] = [Product,"http://amzn.to/2nFNFTl","https://images-na.ssl-images-amazon.com/images/I/518unQP%2Bo3L._SL160_.jpg"];
 
+
+
+content["Bitter-taste"] = [];
 //Bitter taste
-Bitter taste[0,];
-Bitter taste[1,];
-Bitter taste[2,];
-Bitter taste[3,];
-Bitter taste[4,];
+Bitter taste[0];
+Bitter taste[1];
+Bitter taste[2];
+Bitter taste[3];
+Bitter taste[4];
 			
-//Morning Person
-Morning Person[0,];
-Morning Person[1,];
-Morning Person[2,];
-Morning Person[3,];
-Morning Person[4,];
 
+			 
+			 
+		
+content["Morning-Person"] = [];			 
+//Morning Person
+Morning Person[0];
+Morning Person[1];
+Morning Person[2];
+Morning Person[3];
+Morning Person[4];
+
+			   
+			   
+content["Novelty-seeking"] = [];
 //Novelty seeking
-Novelty seeking[0,];
-Novelty seeking[1,];
-Novelty seeking[2,];
-Novelty seeking[3,];
-Novelty seeking[4,];
+Novelty seeking[0];
+Novelty seeking[1];
+Novelty seeking[2];
+Novelty seeking[3];
+Novelty seeking[4];
 				
+				
+				
+				
+content["Openness"] = [];				
 //openness
 Openness[0,];
 Openness[1,];
@@ -105,30 +136,47 @@ Openness[2,];
 Openness[3,];
 Openness[4,];
 
+		 
+		 
+		 
+		 
+content["Red-Wine-restaurant"] = [];
 //red wine liking
-Red wine liking[0,Misc.,"http://sebastianositalianfood.com/blog/","https://media-cdn.tripadvisor.com/media/photo-s/0d/d9/25/80/traditional-italian-restaurant.jpg"];
-Red wine liking[1,];
-Red wine liking[2,"https://www.nomadicmatt.com/travel-guides/italy-travel-tips/","https://www.thetimes.co.uk/imageserver/image/methode%2Ftimes%2Fprod%2Fweb%2Fbin%2Fc234fa2c-d80d-11e6-9063-500e6740fc32.jpg?crop=1407%2C792%2C84%2C109&resize=685"];
-Red wine liking[3,""];
-Red wine liking[4,"http://vinehopper.co.za/","https://www.roomsforafrica.com/new-gallery/boschendalwinefarm_4-x_large.jpg"];
+Red wine liking[0] = [Misc.,"http://sebastianositalianfood.com/blog/","https://media-cdn.tripadvisor.com/media/photo-s/0d/d9/25/80/traditional-italian-restaurant.jpg"];
+Red wine liking[1] = [];
+Red wine liking[2] = ["https://www.nomadicmatt.com/travel-guides/italy-travel-tips/","https://www.thetimes.co.uk/imageserver/image/methode%2Ftimes%2Fprod%2Fweb%2Fbin%2Fc234fa2c-d80d-11e6-9063-500e6740fc32.jpg?crop=1407%2C792%2C84%2C109&resize=685"];
+Red wine liking[3] = [""];
+Red wine liking[4] = ["http://vinehopper.co.za/","https://www.roomsforafrica.com/new-gallery/boschendalwinefarm_4-x_large.jpg"];
 				
-//white wine liking
-white wine liking[0,];
-white wine liking[1,];
-white wine liking[2,];
-white wine liking[3,];
-white wine liking[4,];
 
+
+
+
+content["white-wine-liking"] = [];
+//white wine liking
+white wine liking[0];
+white wine liking[1];
+white wine liking[2];
+white wine liking[3];
+white wine liking[4];
+
+				  
+				  
+content["sleep-duration"] = [];
 //sleep duration
-Sleep duration[0,];
-Sleep duration[1,];
-Sleep duration[2,];
-Sleep duration[3,];
-Sleep duration[4,];
+Sleep duration[0];
+Sleep duration[1];
+Sleep duration[2];
+Sleep duration[3];
+Sleep duration[4];
 			   
+			   
+			   
+			   
+content["High-Cholestrerol"] = [];
 //High-Cholestrerol
-High-Cholestrerol[0,];
-High-Cholestrerol[1,];
-High-Cholestrerol[2,];
-High-Cholestrerol[3,];
-High-Cholestrerol[4,];
+High-Cholestrerol[0];
+High-Cholestrerol[1];
+High-Cholestrerol[2];
+High-Cholestrerol[3];
+High-Cholestrerol[4];
