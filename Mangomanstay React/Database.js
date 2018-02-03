8b64ba089a765fd981341c81a6fa9cf6552acef4
jongan69 <jonathang132298@gmail.com>
2018-02-03 14:14:49 -0500

    y

---
+++ Mangomanstay React/Database.js
@@ -0,0 +1,133 @@
+// Database of Geneome Traits and personalizations
+// Setup: Trait[Rating 0-4,category of relation, link,photo];
+//Categories: Food&beverage, Product recomendation, Things to-do, Travel Tip, Misc.
+
+
+//Alcohol
+Alcohol drinking behavior[0,Food&beverage,"www.tablespoon.com/posts/20-non-alcoholic-party-drinks-everyone-will-love","https://i.pinimg.com/736x/34/06/39/340639697873d15dd0f9006d5e384690--juice-drinks-drinks-alcohol.jpg"];
+Alcohol drinking behavior[1,Food&Beverage,"www.sheknows.com/health-and-wellness/articles/1090849/healthiest-summer-cocktails",];
+Alcohol drinking behavior[2,];
+Alcohol drinking behavior[3,Things to-do,"https://visitsedona.com/blog/wining-and-dining/","https://redsedona.com/wp-content/uploads/revslider/home/555norbie-2017-012.jpg"];
+Alcohol drinking behavior[4,Food&Beverage,"https://www.cruisecritic.com/articles.cfm?ID=1470","http://cdn.newsapi.com.au/image/v1/4c5991420d00b52425d252b0407f6f11"];
+	
+
+//Anger
+Anger[0,Things to-do,"https://disneyworld.disney.go.com/planning-guides/in-depth-advice/first-trip/","https://s-media-cache-ak0.pinimg.com/originals/a0/b4/1a/a0b41ac2524b2ab9247232a6740f4a07.jpg"];
+Anger[1,Things to-do,"https://www.infinityfitness.com/news-and-store/blog/cruise-stress-relief-options/","http://www.trbimg.com/img-54c132de/turbine/os-frozen-disney-cruise-line-20150122"];
+Anger[2,Travel Tip,"http://www.trbimg.com/img-54c132de/turbine/os-frozen-disney-cruise-line-20150122","http://www.vacationexpress.com/images/smile-banner.jpg"];
+Anger[3,Travel Tip,"https://www.huffingtonpost.com/entry/how-to-have-a-stress-free-family-vacation_us_58f82f4be4b0b6ca13416125","https://img.huffingtonpost.com/asset/5903a1312600001800c48543.jpg?ops=scalefit_820_noupscale"];
+Anger[4,Things to-do,"https://blog.freepeople.com/2013/09/blogger-diary-nature-walk/","http://www.connerprairie.org/Places-To-Explore/Images-/Nature-Walk/DSC_1602.jpg"];
+
+
+//Caffeine Consumption
+Caffeine Consumption[0,Food&Beverage,"https://www.cntraveler.com/stories/2015-01-03/americas-10-best-juice-bars-for-your-new-years-cleanse","https://i1.wp.com/www.homeandheavens.com/wp-content/uploads/2014/04/Island-juice-bar-designs_2.jpg?w=600"];
+Caffeine Consumption[1,Food&Beverage,"https://www.huffingtonpost.com/food-wine/best-soda-fountains-in-th_b_5890634.html","http://dallas.culturemap.com/thumbnail/600x600/photos/2013/04/12/Soda-Bar-at-NYLO-South-Side_170955.jpg"];
+Caffeine Consumption[2,Products,"https://www.consumerreports.org/cro/coffee-makers.htm","http://s3.amazonaws.com/digitaltrends-uploads-prod/2016/11/best-coffee-makers.jpg"];
+Caffeine Consumption[3,Food&Beverage,"http://www.travelchannel.com/interests/food-and-drink/photos/worlds-best-cafes-for-coffee-lovers","http://41.media.tumblr.com/5df0747728a28202a62845ef96ead780/tumblr_nikzlsQA9p1tu9sqto1_r1_500.png"];
+Caffeine Consumption[4,,"",""];
+
+
+//Depression
+Depression[0,Things to-do,"https://www.volunteerforever.com/article_post/2017-best-volunteer-abroad-programs-organizations-projects","https://www.professionalgrantwriter.org/wp-content/uploads/2011/07/volunteer-hours-budget-nonprofit.jpg"];
+Depression[1,Food&Beverage,"https://makeyourbodywork.com/best-healthy-food-blogs/","https://www.naturalproductsinsider.com/~/media/8bfde384f9ea49dda5dd8a5205d583f4.ashx"];
+Depression[2,Things to-do,"https://www.prevention.com/fitness/14-walking-workouts-to-burn-fat-and-boost-energy","https://img.aws.livestrongcdn.com/ls-article-image-673/ds-photo/getty/article/81/16/78616903.jpg"];
+Depression[3,Things to-do,"blog.anytimefitness.com/","http://www.tyrolcleaning.com/wp-content/uploads/2012/10/pittsburgh-gym-fitness-cleaning.jpg"];
+Depression[4,Things to-do,"http://trupanion.com/blog/2017/11/7-innovative-therapy-dog-programs/","https://www.gannett-cdn.com/-mm-/83ade179910f010dab8e5f2674eb2a62ea7f7a81/c=169-1205-1446-1927&r=x803&c=1600x800/local/-/media/2015/12/07/Hattiesburg/Hattiesburg/635851049992401523-Puppy-Therapy-7.jpg"];
+		
+//Excessive Daytime Sleepiness
+Excessive Daytime Sleepiness[0,];
+Excessive Daytime Sleepiness[1,];
+Excessive Daytime Sleepiness[2,];
+Excessive Daytime Sleepiness[3,];
+Excessive Daytime Sleepiness[4,];
+
+//Extraversion
+Extraversion[0,Misc,"https://www.quietrev.com/these-travel-destinations-prove-that-quiet-doesnt-mean-boring/",];
+Extraversion[1,];
+Extraversion[2,];
+Extraversion[3,];
+Extraversion[4,];
+			 
+//Gambling
+Gambling[0,];
+Gambling[1,];
+Gambling[2,];
+Gambling[3,];
+Gambling[4,];
+		 
+//Carbohydrate Intake
+Carbohydrate Intake[0,];
+Carbohydrate Intake[1,];
+Carbohydrate Intake[2,];
+Carbohydrate Intake[3,];
+Carbohydrate Intake[4,];
+
+//Nausea
+Nausea[0,];
+Nausea[1,];
+Nausea[2,];
+Nausea[3,];
+Nausea[4,];
+	   
+//Skin Pigmentation
+Skin Pigmentation[0,];
+Skin Pigmentation[1,];
+Skin Pigmentation[2,];
+Skin Pigmentation[3,];
+Skin Pigmentation[4,];
+
+//Bitter taste
+Bitter taste[0,];
+Bitter taste[1,];
+Bitter taste[2,];
+Bitter taste[3,];
+Bitter taste[4,];
+			
+//Morning Person
+Morning Person[0,];
+Morning Person[1,];
+Morning Person[2,];
+Morning Person[3,];
+Morning Person[4,];
+
+//Novelty seeking
+Novelty seeking[0,];
+Novelty seeking[1,];
+Novelty seeking[2,];
+Novelty seeking[3,];
+Novelty seeking[4,];
+				
+//openness
+Openness[0,];
+Openness[1,];
+Openness[2,];
+Openness[3,];
+Openness[4,];
+
+//red wine liking
+Red wine liking[0,];
+Red wine liking[1,];
+Red wine liking[2,];
+Red wine liking[3,];
+Red wine liking[4,];
+				
+//white wine liking
+white wine liking[0,];
+white wine liking[1,];
+white wine liking[2,];
+white wine liking[3,];
+white wine liking[4,];
+
+//sleep duration
+Sleep duration[0,];
+Sleep duration[1,];
+Sleep duration[2,];
+Sleep duration[3,];
+Sleep duration[4,];
+			   
+//High-Cholestrerol
+High-Cholestrerol[0,];
+High-Cholestrerol[1,];
+High-Cholestrerol[2,];
+High-Cholestrerol[3,];
+High-Cholestrerol[4,];
--- Mangomanstay React/src/App.js
+++ Mangomanstay React/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-//import logo from './logo.svg';
-//import './App.css';
+import ReactDOM from 'react-dom';
+import logo from './logo.svg';
+import './App.css';
 //import axios from 'axios';
 
-//This the example code provided from genomelink.io for API in node js 
+//This the example code provided  from genomelink.io for API in node js 
 //const request = require("request");
 
-//const token = "GENOMELINKTEST";
+// const apikey = 'GENOMELINKTEST';
 //const options = { headers: { authorization: `Bearer ${token}` }, json: true };
 
 //const name = 'eye-color';
@@ -22,14 +23,14 @@ import React, { Component } from 'react';
 
 
 //This is the working React code that when run displays basic text on the server it runs on which is locally at the moment
-
+React.render(<App/>, document.getElementById('root'));
 class App extends Component {
   render() {
     return (
       <div className="website">
-        <header className="Website Header">
-          
-          <h1 className="website-title">MANGOMANSTAY</h1></header>
+        <header className="Website Header">  
+        <h1 className="website-title">MANGOMANSTAY</h1></header>
+		
       
       </div>
     );
@@ -38,9 +39,11 @@ class App extends Component {
 export default App;
 //This is my attempt at building an API call using axios and the example link
 		
-//		axios.get('https://genomicexplorer.io/v1/reports/${name}/?population=${population}'
-//  .then(function(response) {
-//    console.log(response);
-//})
-//  
+/*a mmmmmmmmmxios.get('https://genomicexplorer.io/v1/reports/${name}/?population=${population}')
+  .then(function(response) {
+			var gs = response.data.data;
+			var url = gs
+    console.log(URL);
+})*/
+  
 
