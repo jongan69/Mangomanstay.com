7244eea98fbb5a894180501790fe4966fa2e3fea
jongan69 <jonathang132298@gmail.com>
2018-02-03 14:43:10 -0500

    axios

--- Mangomanstay React/Database.js
+++ Mangomanstay React/Database.js
@@ -2,21 +2,22 @@
 // Setup: Trait[Rating 0-4,category of relation, link,photo];
 //Categories: Food&beverage, Product recomendation, Things to-do, Travel Tip, Misc.
 
-
+var content = [];
+content["alcohol-drinking-behavior"] = [];
 //Alcohol
-Alcohol drinking behavior[0,Food&beverage,"www.tablespoon.com/posts/20-non-alcoholic-party-drinks-everyone-will-love","https://i.pinimg.com/736x/34/06/39/340639697873d15dd0f9006d5e384690--juice-drinks-drinks-alcohol.jpg"];
-Alcohol drinking behavior[1,Food&Beverage,"www.sheknows.com/health-and-wellness/articles/1090849/healthiest-summer-cocktails",];
-Alcohol drinking behavior[2,];
-Alcohol drinking behavior[3,Things to-do,"https://visitsedona.com/blog/wining-and-dining/","https://redsedona.com/wp-content/uploads/revslider/home/555norbie-2017-012.jpg"];
-Alcohol drinking behavior[4,Food&Beverage,"https://www.cruisecritic.com/articles.cfm?ID=1470","http://cdn.newsapi.com.au/image/v1/4c5991420d00b52425d252b0407f6f11"];
+content["alcohol-drinking-behavior"][0] =["Food&beverage","www.tablespoon.com/posts/20-non-alcoholic-party-drinks-everyone-will-love","https://i.pinimg.com/736x/34/06/39/340639697873d15dd0f9006d5e384690--juice-drinks-drinks-alcohol.jpg"];
+Alcohol drinking behavior[1]= ["Food&Beverage","www.sheknows.com/health-and-wellness/articles/1090849/healthiest-summer-cocktails",];
+Alcohol drinking behavior[2]= [;
+Alcohol drinking behavior[3]= ["Things to-do","https://visitsedona.com/blog/wining-and-dining/","https://redsedona.com/wp-content/uploads/revslider/home/555norbie-2017-012.jpg"];
+Alcohol drinking behavior[4]= ["Food&Beverage","https://www.cruisecritic.com/articles.cfm?ID=1470","http://cdn.newsapi.com.au/image/v1/4c5991420d00b52425d252b0407f6f11"];
 	
 
 //Anger
-Anger[0,Things to-do,"https://disneyworld.disney.go.com/planning-guides/in-depth-advice/first-trip/","https://s-media-cache-ak0.pinimg.com/originals/a0/b4/1a/a0b41ac2524b2ab9247232a6740f4a07.jpg"];
-Anger[1,Things to-do,"https://www.infinityfitness.com/news-and-store/blog/cruise-stress-relief-options/","http://www.trbimg.com/img-54c132de/turbine/os-frozen-disney-cruise-line-20150122"];
-Anger[2,Travel Tip,"http://www.trbimg.com/img-54c132de/turbine/os-frozen-disney-cruise-line-20150122","http://www.vacationexpress.com/images/smile-banner.jpg"];
-Anger[3,Travel Tip,"https://www.huffingtonpost.com/entry/how-to-have-a-stress-free-family-vacation_us_58f82f4be4b0b6ca13416125","https://img.huffingtonpost.com/asset/5903a1312600001800c48543.jpg?ops=scalefit_820_noupscale"];
-Anger[4,Things to-do,"https://blog.freepeople.com/2013/09/blogger-diary-nature-walk/","http://www.connerprairie.org/Places-To-Explore/Images-/Nature-Walk/DSC_1602.jpg"];
+Anger[0] = ["Things to-do","https://disneyworld.disney.go.com/planning-guides/in-depth-advice/first-trip/","https://s-media-cache-ak0.pinimg.com/originals/a0/b4/1a/a0b41ac2524b2ab9247232a6740f4a07.jpg"];
+Anger[1] = ["Things to-do","https://www.infinityfitness.com/news-and-store/blog/cruise-stress-relief-options/","http://www.trbimg.com/img-54c132de/turbine/os-frozen-disney-cruise-line-20150122"];
+Anger[2] = [Travel Tip,"http://www.trbimg.com/img-54c132de/turbine/os-frozen-disney-cruise-line-20150122","http://www.vacationexpress.com/images/smile-banner.jpg"];
+Anger[3] = [Travel Tip,"https://www.huffingtonpost.com/entry/how-to-have-a-stress-free-family-vacation_us_58f82f4be4b0b6ca13416125","https://img.huffingtonpost.com/asset/5903a1312600001800c48543.jpg?ops=scalefit_820_noupscale"];
+Anger[4] = ["Things to-do","https://blog.freepeople.com/2013/09/blogger-diary-nature-walk/","http://www.connerprairie.org/Places-To-Explore/Images-/Nature-Walk/DSC_1602.jpg"];
 
 
 //Caffeine Consumption
@@ -35,15 +36,15 @@ Depression[3,Things to-do,"blog.anytimefitness.com/","http://www.tyrolcleaning.c
 Depression[4,Things to-do,"http://trupanion.com/blog/2017/11/7-innovative-therapy-dog-programs/","https://www.gannett-cdn.com/-mm-/83ade179910f010dab8e5f2674eb2a62ea7f7a81/c=169-1205-1446-1927&r=x803&c=1600x800/local/-/media/2015/12/07/Hattiesburg/Hattiesburg/635851049992401523-Puppy-Therapy-7.jpg"];
 		
 //Excessive Daytime Sleepiness
-Excessive Daytime Sleepiness[0,];
-Excessive Daytime Sleepiness[1,];
-Excessive Daytime Sleepiness[2,];
+Excessive Daytime Sleepiness[0,Travel Tip,"https://www.webmd.com/sleep-disorders/features/natural-tips-sleepiness#1","https://33q47o1cmnk34cvwth15pbvt120l-wpengine.netdna-ssl.com/wp-content/uploads/insomnia.jpg"];
+Excessive Daytime Sleepiness[1,Things to-do,"https://greatist.com/fitness/take-it-easy-21-unexpected-low-impact-workouts","http://www.qtfitness.me/wp-content/uploads/2014/10/workout-park-friends-1.jpg"];
+Excessive Daytime Sleepiness[2,Food&Beverage,"https://www.livescience.com/36579-eat-meals.html","https://static.independent.co.uk/s3fs-public/styles/article_small/public/thumbnails/image/2017/11/15/15/eating-quickly.jpg"];
 Excessive Daytime Sleepiness[3,];
 Excessive Daytime Sleepiness[4,];
 
 //Extraversion
-Extraversion[0,Misc,"https://www.quietrev.com/these-travel-destinations-prove-that-quiet-doesnt-mean-boring/",];
-Extraversion[1,];
+Extraversion[0,Misc,"https://www.quietrev.com/these-travel-destinations-prove-that-quiet-doesnt-mean-boring/","https://33q47o1cmnk34cvwth15pbvt120l-wpengine.netdna-ssl.com/wp-content/uploads/insomnia.jpg"];
+Extraversion[1,Things to-do,"https://greatist.com/fitness/take-it-easy-21-unexpected-low-impact-workouts","http://www.qtfitness.me/wp-content/uploads/2014/10/workout-park-friends-1.jpg"];
 Extraversion[2,];
 Extraversion[3,];
 Extraversion[4,];
@@ -70,11 +71,11 @@ Nausea[3,];
 Nausea[4,];
 	   
 //Skin Pigmentation
-Skin Pigmentation[0,];
-Skin Pigmentation[1,];
-Skin Pigmentation[2,];
-Skin Pigmentation[3,];
-Skin Pigmentation[4,];
+Skin Pigmentation[0,Product,"http://amzn.to/2nFNFTl","https://images-na.ssl-images-amazon.com/images/I/518unQP%2Bo3L._SL160_.jpg"];
+Skin Pigmentation[1,Product,"http://amzn.to/2nFNFTl","https://images-na.ssl-images-amazon.com/images/I/518unQP%2Bo3L._SL160_.jpg"];
+Skin Pigmentation[2,Product,"http://amzn.to/2nFNFTl","https://images-na.ssl-images-amazon.com/images/I/518unQP%2Bo3L._SL160_.jpg"];
+Skin Pigmentation[3,Product,"http://amzn.to/2nFNFTl","https://images-na.ssl-images-amazon.com/images/I/518unQP%2Bo3L._SL160_.jpg"];
+Skin Pigmentation[4,Product,"http://amzn.to/2nFNFTl","https://images-na.ssl-images-amazon.com/images/I/518unQP%2Bo3L._SL160_.jpg"];
 
 //Bitter taste
 Bitter taste[0,];
@@ -105,11 +106,11 @@ Openness[3,];
 Openness[4,];
 
 //red wine liking
-Red wine liking[0,];
+Red wine liking[0,Misc.,"http://sebastianositalianfood.com/blog/","https://media-cdn.tripadvisor.com/media/photo-s/0d/d9/25/80/traditional-italian-restaurant.jpg"];
 Red wine liking[1,];
-Red wine liking[2,];
-Red wine liking[3,];
-Red wine liking[4,];
+Red wine liking[2,"https://www.nomadicmatt.com/travel-guides/italy-travel-tips/","https://www.thetimes.co.uk/imageserver/image/methode%2Ftimes%2Fprod%2Fweb%2Fbin%2Fc234fa2c-d80d-11e6-9063-500e6740fc32.jpg?crop=1407%2C792%2C84%2C109&resize=685"];
+Red wine liking[3,""];
+Red wine liking[4,"http://vinehopper.co.za/","https://www.roomsforafrica.com/new-gallery/boschendalwinefarm_4-x_large.jpg"];
 				
 //white wine liking
 white wine liking[0,];
